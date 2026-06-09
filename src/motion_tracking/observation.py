65116ef1e464433fc92a463c768c91dc7022da15
lhx <wizardhxliang@gmail.com>
2026-06-09 13:26:43 +0800

    A2: isolate BM critic body representation

--- src/motion_tracking/observation.py
+++ src/motion_tracking/observation.py
@@ -136,6 +136,42 @@ class body_pose_b(Observation):
     ):
         super().__init__(env)
         self.asset = self.env.scene.articulations["robot"]
+        self.body_ids, self.body_names = find_bodies(self.asset, body_names)
+        anchor_ids, _ = find_bodies(self.asset, anchor_body_name)
+        self.body_ids = torch.tensor(self.body_ids, device=self.device)
+        self.anchor_id = anchor_ids[0]
+
+    def compute(self) -> torch.Tensor:
+        body_pos_w = self.asset.data.body_link_pos_w[:, self.body_ids]
+        body_quat_w = self.asset.data.body_link_quat_w[:, self.body_ids]
+        anchor_pos_w = self.asset.data.body_link_pos_w[:, self.anchor_id]
+        anchor_quat_w = self.asset.data.body_link_quat_w[:, self.anchor_id]
+
+        rel_pos = quat_rotate_inverse(
+            anchor_quat_w.unsqueeze(1),
+            body_pos_w - anchor_pos_w.unsqueeze(1),
+        )
+        rel_quat = quat_mul(
+            quat_conjugate(anchor_quat_w).unsqueeze(1),
+            body_quat_w,
+        )
+        rel_rotmat = matrix_from_quat(rel_quat).reshape(
+            self.num_envs,
+            len(self.body_ids),
+            9,
+        )
+        return torch.cat([rel_pos, rel_rotmat], dim=-1).reshape(self.num_envs, -1)
+
+
+class body_pose_bm(body_pose_b):
+    def __init__(
+        self,
+        env,
+        body_names: str | list[str],
+        anchor_body_name: str,
+    ):
+        Observation.__init__(self, env)
+        self.asset = self.env.scene.articulations["robot"]
         self.body_ids, self.body_names = self.asset.find_bodies(
             body_names,
             preserve_order=True,
